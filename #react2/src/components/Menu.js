af56c603e473d7576470ba0c995c9a30a1810ee5
hyejin192 <129017064+hyejin192@users.noreply.github.com>
2023-05-31 17:05:43 +0900

    Add files via upload

---
+++ #react2/src/components/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Menu = ({name}) => {
+  return (
+    <div>{name}Menu</div>
+  )
+}
+
+export default Menu
---
+++ #react2/src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react"; /* 💖 */
+
+const Header = function({num,color}) {
+    return (
+        <div>나는{num}번 {color}머리</div>
+    )
+}
+
+export default Header; /* ✔ */
